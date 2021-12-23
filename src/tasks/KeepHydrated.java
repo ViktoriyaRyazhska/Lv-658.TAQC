70af474e45e68f6cbcad4ebda5dc57e52c8ec038
Дінара Мустафаєва <dina.must44@gmail.com>
2021-12-23 12:22:35 +0200

    Added Keep Hydrated Task

--- src/tasks/KeepHydrated.java
+++ src/tasks/KeepHydrated.java
@@ -1,4 +1,27 @@
 package tasks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class KeepHydrated {
+
+    public static int numberOfLitres(double time) {
+        int litres;
+        litres = (int)time /2;
+        return litres;
+    }
+
+    public static void task(BufferedReader br) {
+        System.out.println("Please, enter the time:");
+        try {
+            double time = Float.parseFloat(br.readLine());
+            int litres = numberOfLitres(time);
+            System.out.printf("time = %f ----> litres = %d", time, litres);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("You are expected to enter time with a number.");
+        }
+    }
+
 }
