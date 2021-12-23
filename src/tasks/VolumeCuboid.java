8861c4abf6caa9784593b70d555bade7fa13d7a0
Дінара Мустафаєва <dina.must44@gmail.com>
2021-12-23 09:48:33 +0200

    Added Volume of a Cuboid Task

--- src/tasks/VolumeCuboid.java
+++ src/tasks/VolumeCuboid.java
@@ -1,4 +1,28 @@
 package tasks;
 
 public class VolumeCuboid {
+
+    public static double calculateVolume(double length, double width, double height) {
+        double volume;
+        volume = length * width * height;
+        return volume;
+    }
+
+    public static void task(BufferedReader br) {
+        System.out.println("Please, enter sizes:");
+        try {
+            double length = Double.parseDouble(br.readLine());
+            double width = Double.parseDouble(br.readLine());
+            double height = Double.parseDouble(br.readLine());
+            double volume = calculateVolume(length, width, height);
+            if (length < 0 || width < 0 || height < 0) {
+                throw new IllegalArgumentException("Only Positive Numbers & no Letters Please!");
+            }
+            System.out.printf("length = %f; width = %f; height = %f ----> volume = %f", length, width, height, volume);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
