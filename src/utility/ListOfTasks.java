4490f8e782e2351819da0be90502d842ca68f9db
Roman Kaniuka <kanuka878@gmail.com>
2021-12-24 01:39:00 +0200

    Completed class 'ListOfTasks' which contain list of tasks

--- src/utility/ListOfTasks.java
+++ src/utility/ListOfTasks.java
@@ -1,4 +1,32 @@
 package utility;
 
 public class ListOfTasks {
+    public static String listOfTasks() {
+        String list = "1. ArtificialRain\n" +
+                "2. LookingBenefactor\n" +
+                "3. TrailingZeros\n" +
+                "4. StringToNumber\n" +
+                "5. TheSmallest\n" +
+                "6. PerimeterSquares\n" +
+                "7. KeepHydrated\n" +
+                "8. MilesToKilometers\n" +
+                "9. WillsonPrimes\n" +
+                "10. Rainfall\n" +
+                "11. FormattingDecimal\n" +
+                "12. SquareRoot\n" +
+                "13. VolumeCuboid\n" +
+                "14. WhichX\n" +
+                "15. BuildCubes\n" +
+                "16. Vasya\n" +
+                "17. BalanceChecking\n" +
+                "18. SumOfTerm\n" +
+                "19. GapPrimes\n" +
+                "20. HelpBookseller\n" +
+                "21. CountPositive\n" +
+                "22. IsDivisible\n" +
+                "23. RankingNBA\n" +
+                "24. FloatingPoint";
+
+        return list;
+    }
 }
