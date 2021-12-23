af8d4f4d5573c9faf4076d15c0c44f0c073e9539
Roman Kaniuka <kanuka878@gmail.com>
2021-12-24 01:45:25 +0200

    Modified 'ListOfTasks.java' with spliting list and adding getters

--- src/utility/ListOfTasks.java
+++ src/utility/ListOfTasks.java
@@ -1,7 +1,7 @@
 package utility;
 
 public class ListOfTasks {
-    public static String listOfTasks() {
+    private static String firstList() {
         String list = "1. ArtificialRain\n" +
                 "2. LookingBenefactor\n" +
                 "3. TrailingZeros\n" +
@@ -9,24 +9,47 @@ public class ListOfTasks {
                 "5. TheSmallest\n" +
                 "6. PerimeterSquares\n" +
                 "7. KeepHydrated\n" +
-                "8. MilesToKilometers\n" +
-                "9. WillsonPrimes\n" +
-                "10. Rainfall\n" +
-                "11. FormattingDecimal\n" +
-                "12. SquareRoot\n" +
-                "13. VolumeCuboid\n" +
-                "14. WhichX\n" +
-                "15. BuildCubes\n" +
-                "16. Vasya\n" +
-                "17. BalanceChecking\n" +
-                "18. SumOfTerm\n" +
-                "19. GapPrimes\n" +
-                "20. HelpBookseller\n" +
-                "21. CountPositive\n" +
-                "22. IsDivisible\n" +
-                "23. RankingNBA\n" +
-                "24. FloatingPoint";
+                "8. MilesToKilometers" +
+                "9. WillsonPrimes";
 
         return list;
     }
+
+    private static String secondList() {
+        String list = "1. Rainfall\n" +
+                "2. FormattingDecimal\n" +
+                "3. SquareRoot\n" +
+                "4. VolumeCuboid\n" +
+                "5. WhichX\n" +
+                "6. BuildCubes\n" +
+                "7. Vasya\n" +
+                "8. BalanceChecking\n" +
+                "9. SumOfTerm";
+
+        return list;
+    }
+
+    private static String thirdList() {
+        String list = "1. GapPrimes\n" +
+                "2. HelpBookseller\n" +
+                "3. CountPositive\n" +
+                "4. IsDivisible\n" +
+                "5. RankingNBA\n" +
+                "6. FloatingPoint";
+
+        return list;
+    }
+
+    public static String getFirstList() {
+        return firstList();
+    }
+
+    public static String getSecondList() {
+        return secondList();
+    }
+
+    public static String getThirdList() {
+        return thirdList();
+    }
+
 }
