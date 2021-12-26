8c2cd729362a6ca3cfada5094bea470697a21183
Дінара Мустафаєва <dina.must44@gmail.com>
2021-12-26 20:29:31 +0200

    Added task 'Convert a String to a Number'

--- src/tasks/StringToNumber.java
+++ src/tasks/StringToNumber.java
@@ -1,4 +1,29 @@
 package tasks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class StringToNumber {
+
+    public static int convert(String str) {
+        boolean flag;
+        Integer number = Integer.valueOf(str);
+        flag = isNumeric(str);
+        if (flag) {
+            return number;
+        } else {
+            throw new NumberFormatException("No Letters Please!");
+        }
+    }
+
+    private static boolean isNumeric(String str) {
+        return str != null && str.matches("[0-9.]+");
+    }
+
+    public static void task(BufferedReader br) throws IOException {
+        System.out.println("Please, enter the number:");
+        String numberStr = br.readLine();
+        int number = convert(numberStr);
+        System.out.printf(" '%s' ----> %d ", numberStr, number);
+    }
 }
