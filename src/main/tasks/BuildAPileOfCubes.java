b31ec903f648845f145a203f3dc7b19e8d348a28
setupb <setupb1@gmail.com>
2021-12-29 19:18:20 +0200

    add BuildAPileOfCubes.java

--- src/main/tasks/BuildAPileOfCubes.java
+++ src/main/tasks/BuildAPileOfCubes.java
@@ -2,3 +2,20 @@ package tasks;
 
 public class BuildAPileOfCubes {
 }
+    public static long findNb(long m) {
+        long mm = 0;
+        long n = 0;
+        while(mm < m){
+            n = n + 1;
+            mm = mm + n * n * n;
+        }
+        if(mm == m){
+            return n;
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        findNb(10);
+        System.out.println(findNb(1071225));
+    }
