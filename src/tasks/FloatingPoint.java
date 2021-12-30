11aaa6142262558d4b2846b23f87c99848de1b46
OlenaKulchytska <olenakulchutska3@gmail.com>
2021-12-30 11:12:44 +0200

    add task Floating Point Approximation

--- src/tasks/FloatingPoint.java
+++ src/tasks/FloatingPoint.java
@@ -1,4 +1,22 @@
 package tasks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class FloatingPoint {
+
+    private static double f(double x) {
+        return Math.expm1(Math.log1p(x) / 2);
+    }
+
+    public static void task(BufferedReader br) {
+        System.out.println("Please, enter x for approximation of function:");
+        try {
+            double x = Double.parseDouble(br.readLine());
+
+            System.out.println("F(x) = "+ f(x));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
