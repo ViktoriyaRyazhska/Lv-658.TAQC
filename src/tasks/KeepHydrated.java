c31ec4d2e5d56f040c17a1a7db92ce894a66e9a9
Дінара Мустафаєва <dina.must44@gmail.com>
2021-12-30 16:08:27 +0200

    Exceptions to Keep Hydrated added

--- src/tasks/KeepHydrated.java
+++ src/tasks/KeepHydrated.java
@@ -5,25 +5,27 @@ import java.io.IOException;
 
 public class KeepHydrated {
 
-    public static int numberOfLitres(double time) {
+    private static int numberOfLitres(double time) {
         int litres;
-        litres = (int)time /2;
+        litres = (int) time / 2;
         return litres;
     }
 
-    public static void task(BufferedReader br) throws IOException{
+    public static void task(BufferedReader br) {
         System.out.println("Please, enter the time:");
         try {
             double time = Float.parseFloat(br.readLine());
-            if (time < 0 ){
-                throw new IllegalArgumentException("Only Positive Numbers & no Letters Please!");
+            if (time < 0) {
+                throw new IllegalArgumentException("Only Positive Numbers!");
             }
             int litres = numberOfLitres(time);
             System.out.printf("time = %.2f ----> litres = %d", time, litres);
         } catch (IOException e) {
             e.printStackTrace();
+            task(br);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            task(br);
         }
     }
 }
