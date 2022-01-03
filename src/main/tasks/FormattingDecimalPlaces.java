4af26e931580e6ec8e94a57f8bef2774f4193c81
setupb <setupb1@gmail.com>
2022-01-03 17:49:17 +0200

    update FormattingDecimalPlaces.java and FormattingDecimalPlacesTest.java

--- src/main/tasks/FormattingDecimalPlaces.java
+++ src/main/tasks/FormattingDecimalPlaces.java
@@ -1,17 +1,30 @@
-package tasks;
+package com.company;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 
 public class FormattingDecimalPlaces {
+    public static void conditionOfTask() {
+        System.out.println("Each number should be formatted that it is rounded to " +
+                "two decimal places. You don't need to check whether the input is a valid " +
+                "number because only valid numbers are used in the tests." );
+    }
 
-    public static void main(String args[]){
+
+    public static void runTask8(BufferedReader reader)throws IOException{
+        conditionOfTask();
         String a ="";
-        Test4 start = new Test4();
+        FormattingDecimalPlaces start = new FormattingDecimalPlaces();
         Scanner scn = new Scanner(System.in);
         System.out.println("enter double number like 2,222 or 2222");
 
-        String i = scn.nextLine();
+//        String i = scn.nextLine();
         try {
+            final String i = reader.readLine();
             Double i1 = Double.parseDouble(i);
             String res = start.FDP(i1);
             System.out.println(res);
@@ -25,4 +38,5 @@ public class FormattingDecimalPlaces {
         String x1 = String.format("%.2f",x);
         return x1;
     }
+//
 }
--- src/test/taskstest/FindNumbersWhichAreDivisibleByGivenNumberTest.java
+++ src/test/taskstest/FindNumbersWhichAreDivisibleByGivenNumberTest.java
@@ -1,4 +1,21 @@
 package taskstest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class FindNumbersWhichAreDivisibleByGivenNumberTest {
+    @Test
+    public void Test4T(){
+        FindDivisibleBy T4T = new FindDivisibleBy();
+        ArrayList x = new ArrayList<>();
+        ArrayList y = new ArrayList<>();
+        x.add(1);
+        x.add(2);
+        x.add(3);
+        x.add(4);
+        y.add(2);
+        ArrayList res = T4T.funct(x,y);
+        Assert.assertEquals("[2,4]",res);
+    }
 }
