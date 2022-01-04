5439d734a20e79918a5726e7044e614f426d758f
Дінара Мустафаєва <dina.must44@gmail.com>
2022-01-04 17:19:26 +0200

    Added vallidate method

--- src/tasks/MilesToKilometers.java
+++ src/tasks/MilesToKilometers.java
@@ -22,21 +22,32 @@ public class MilesToKilometers {
         return bigDecimal.floatValue();
     }
 
+    private static boolean validate(float numberMPG) {
+        boolean flag = true;
+        if (numberMPG < 0) {
+            flag = false;
+            System.out.println("Only Positive Numbers!");
+        }
+        return flag;
+    }
+
     public static void task(BufferedReader br) {
         System.out.println("Please, enter the number of miles per imperial gallon :");
         try {
             float numberMPG = Float.parseFloat(br.readLine());
-            if (numberMPG < 0) {
-                throw new IllegalArgumentException("Only Positive Numbers!");
+            if (validate(numberMPG)) {
+                float numberLPK = mpgToKPL(numberMPG);
+                System.out.println("Miles per Gallon = " + roundFloat(numberMPG, 2));
+                System.out.println("Kilometers per Liter = " + roundFloat(numberLPK, 2));
             }
-            float numberLPK = mpgToKPL(numberMPG);
-            System.out.println("Miles per Gallon = " + roundFloat(numberMPG, 2));
-            System.out.println("Kilometers per Liter = " + roundFloat(numberLPK, 2));
+            else{
+                task(br);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error. Try again.");
             task(br);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println("No letters! You should enter number.");
             task(br);
         }
     }
