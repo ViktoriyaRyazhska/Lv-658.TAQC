a68e1bba4a0d7aa13a8e1e06042d47fe57bffaed
Дінара Мустафаєва <dina.must44@gmail.com>
2022-01-04 17:58:39 +0200

    Added vallidate method

--- src/tasks/StringToNumber.java
+++ src/tasks/StringToNumber.java
@@ -10,24 +10,37 @@ public class StringToNumber {
         return number;
     }
 
+    //^(\+|-)?\d+$
+    //[0-9]+
     private static boolean isNumeric(String str) {
-        return str != null && str.matches("[0-9.]+");
+        return str != null && str.matches("^(\\+|-)?\\d+$");
+    }
+
+    private static boolean validate(String str) {
+        boolean flag = true;
+        if (!isNumeric(str)) {
+            flag = false;
+            System.out.println("Just integers & No Letters Please!");
+        }
+        return flag;
     }
 
     public static void task(BufferedReader br) {
         System.out.println("Please, enter the number:");
         try {
             String numberStr = br.readLine();
-            if (!isNumeric(numberStr)) {
-                throw new IllegalArgumentException("No Letters Please!");
+            if (validate(numberStr)) {
+                int number = convert(numberStr);
+                System.out.printf(" '%s' ----> %d \n", numberStr, number);
             }
-            int number = convert(numberStr);
-            System.out.printf(" '%s' ----> %d ", numberStr, number);
+            else{
+                task(br);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error. Try again.");
             task(br);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println("No letters! You should enter number.");
             task(br);
         }
     }
