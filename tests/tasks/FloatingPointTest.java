bd13ef064d9fb346c0131762bfc57c6ead736871
Дінара Мустафаєва <dina.must44@gmail.com>
2022-01-05 22:39:42 +0200

    Added tests to FloatingPoint

--- tests/tasks/FloatingPointTest.java
+++ tests/tasks/FloatingPointTest.java
@@ -1,7 +1,35 @@
 package tasks;
 
-import static org.testng.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class FloatingPointTest {
 
+    @DataProvider(name = "Test-Data-for-F-FP")
+    public Object[][] dataProviderF() {
+        return new Object[][]{
+                {2.6e-08, 1.2999999915500002e-08},
+                {1.4e-09, 6.99999999755e-10},
+                {5.0e-06, 2.4999968750078126e-06},
+                {2.4e-07, 1.1999999280000085e-07}};
+    }
+
+    @Test(dataProvider = "Test-Data-for-F-FP")
+    public void testPositiveFFP(double input, double result) {
+        Assert.assertEquals(FloatingPoint.getF(input), result);
+    }
+
+    @DataProvider(name = "Test-Data-for-Validate-FP")
+    public Object[][] dataProviderValidateFP() {
+        return new Object[][]{
+                {-3},
+                {-1566},
+                {0}};
+    }
+
+    @Test(dataProvider = "Test-Data-for-Validate-FP")
+    public void testPositiveValidateFP(double input) {
+        Assert.assertFalse(FloatingPoint.getValidate(input));
+    }
 }
