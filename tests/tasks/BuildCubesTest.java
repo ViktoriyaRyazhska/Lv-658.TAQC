313679c40bb5ef0fec5f548771e8305fe08975a8
Дінара Мустафаєва <dina.must44@gmail.com>
2022-01-05 22:01:19 +0200

    Added test to BuildCubes

--- tests/tasks/BuildCubesTest.java
+++ tests/tasks/BuildCubesTest.java
@@ -1,7 +1,23 @@
 package tasks;
 
-import static org.testng.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class BuildCubesTest {
 
+    @DataProvider(name = "Test-Data-for-FindNb")
+    public Object[][] dataProviderPositive() {
+        return new Object[][]{
+                {4183059834009L, 2022},
+                {1071225, 45},
+                {135440716410000L, 4824},
+                {24723578342962L, -1},
+                {91716553919377L, -1}};
+    }
+
+    @Test(dataProvider = "Test-Data-for-FindNb")
+    public void testPositiveFindNb(long input, int result) {
+        Assert.assertEquals(BuildCubes.getFindNb(input), result);
+    }
 }
