2383818388df0e9cdede8981652d27e9560b514d
Дінара Мустафаєва <dina.must44@gmail.com>
2022-01-08 12:32:57 +0200

    Added tests to BalanceChecking

--- src/tasks/BalanceChecking.java
+++ src/tasks/BalanceChecking.java
@@ -1,41 +1,54 @@
 package tasks;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BalanceChecking {
 
-    private static double systemDouble(double d){
-        int i =(int) Math.round(d*100);
-        d=((double)i)/100;
+    private static double systemDouble(double d) {
+        int i = (int) Math.round(d * 100);
+        d = ((double) i) / 100;
         return d;
     }
 
-    private static String deleteNotUsableSymbols(String cB){
+    private static String deleteNotUsableSymbols(String cB) {
         String input;
         input = cB.replaceAll("[^\\p{L}\\p{Z}\\p{N}\\.]", "");
         return input;
     }
 
     private static String balance(String cB) {
-        String[]lines =cB.split("\n");
+        String[] lines = cB.split("\n");
         for (int i = 0; i < lines.length; i++) {
             lines[i] = deleteNotUsableSymbols(lines[i]);
         }
-        double balance= Double.valueOf(lines[0]);
+        double balance = Double.valueOf(lines[0]);
         double sum = 0.0;
         String[] line;
-        StringBuilder out= new StringBuilder();
-        out.append("Original Balance: "+systemDouble(balance)).append("\n");
+        StringBuilder out = new StringBuilder();
+        out.append("Original Balance: " + systemDouble(balance)).append("\n");
         for (int i = 1; i < lines.length; i++) {
             line = lines[i].split(" ");
             sum += Double.parseDouble(line[2]);
-            out.append(line[0]).append(" ").append(line[1].replaceAll("[^\\p{L}\\p{Z}]", "")).append(" ").append(line[2]).append(" Balance ").append(systemDouble(balance = Double.sum(balance,-Double.parseDouble(line[2])))).append("\n");
+            out.append(line[0]).append(" ").append(line[1].replaceAll("[^\\p{L}\\p{Z}]", "")).append(" ").append(line[2]).append(" Balance ").append(systemDouble(balance = Double.sum(balance, -Double.parseDouble(line[2])))).append("\n");
         }
-        double k = (lines.length)-1.0;
+        double k = (lines.length) - 1.0;
         out.append("Total expense ").append(systemDouble(sum)).append("\n");
-        out.append("Average expense ").append(systemDouble(sum/k));
+        out.append("Average expense ").append(systemDouble(sum / k));
         return out.toString();
+    }
+
+    public static double getSystemDouble(double d) {
+        return systemDouble(d);
+    }
+
+    public static String getDeleteNotUsableSymbols(String str) {
+        return deleteNotUsableSymbols(str);
+    }
+
+    public static String getBalance(String str) {
+        return balance(str);
     }
 
     public static void task(BufferedReader br) {
--- tests/tasks/BalanceCheckingTest.java
+++ tests/tasks/BalanceCheckingTest.java
@@ -1,7 +1,51 @@
 package tasks;
 
-import static org.testng.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class BalanceCheckingTest {
 
+    @DataProvider(name = "Test-Data-for-SystemDouble-BC")
+    public Object[][] dataProviderSystemDouble() {
+        return new Object[][]{
+                {87.41, 87.41},
+                {-996.5, -996.5},
+                {856, 856},
+                {0, 0}};
+    }
+
+    @Test(dataProvider = "Test-Data-for-SystemDouble-BC")
+    public void testSystemDoubleBC(double input, double result) {
+        Assert.assertEquals(BalanceChecking.getSystemDouble(input), result);
+    }
+
+    @DataProvider(name = "Test-Data-for-DeleteSymbols-BC")
+    public Object[][] dataProviderDeleteSymbols() {
+        return new Object[][]{
+                {"87___41", "8741"},
+                {"125 Market 125.45\n126 Hardware 34.95", "125 Market 125.45126 Hardware 34.95"}};
+    }
+
+    @Test(dataProvider = "Test-Data-for-DeleteSymbols-BC")
+    public void testDeleteSymbolsBC(String input, String result) {
+        Assert.assertEquals(BalanceChecking.getDeleteNotUsableSymbols(input), result);
+    }
+
+    @DataProvider(name = "Test-Data-for-Balance-BC")
+    public Object[][] dataProviderBalance() {
+        return new Object[][]{
+                {"1000.00\n125 Market 125.45\n126 Hardware 34.95\n127 Video 7.45",
+                        "Original Balance: 1000.0\n" +
+                                "125 Market 125.45 Balance 874.55\n" +
+                                "126 Hardware 34.95 Balance 839.6\n" +
+                                "127 Video 7.45 Balance 832.15\n" +
+                                "Total expense 167.85\n" +
+                                "Average expense 55.95"}};
+    }
+
+    @Test(dataProvider = "Test-Data-for-Balance-BC")
+    public void testBalanceBC(String input, String result) {
+        Assert.assertEquals(BalanceChecking.getBalance(input), result);
+    }
 }
