1cf7bf6c200268469fb019f8ae97f5146cfe1bfe
Roman Kaniuka <kanuka878@gmail.com>
2022-01-09 11:25:11 +0200

    Made small changes

--- tests/tasks/IsDivisibleTest.java
+++ tests/tasks/IsDivisibleTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.nio.BufferOverflowException;
 
 import org.mockito.Mockito;
 
