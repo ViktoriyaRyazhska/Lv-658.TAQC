6aa13e0f69c5054128cbed57a3f170d6644e7b29
Anna Chopyk <annachopuk0@gmail.com>
2022-01-09 11:54:13 +0200

    GapPrimesTest MilesToKilometersTest Mock

--- tests/tasks/SquareRootTest.java
+++ tests/tasks/SquareRootTest.java
@@ -1,10 +1,15 @@
 package tasks;
 
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class SquareRootTest {
 
@@ -15,20 +20,6 @@ public class SquareRootTest {
                 return new Object[][]{{-2}, {-5}, {-3}, {-10}};
             case "isPerfectSquareTrueTest":
                 return new Object[][]{{0}, {1}, {4}, {9}};
-            case "validateSizePositiveTest":
-                return new Object[][]{{2}, {5}, {11}, {4}};
-            case "validateSizeNegativeTest":
-                return new Object[][]{{-3}, {-1}, {-12}};
-            case "validateArrayInputPositiveTest":
-                int[]arr1 = new int[]{1,3,8,0};
-                int[]arr2 = new int[]{2,12,9,3,1,6};
-                int[]arr3 = new int[]{};
-                return new Object[][]{{arr1},{arr2},{arr3}};
-            case "validateArrayInputNegativeTest":
-                int[]arr4 = new int[]{1,3,-8,0};
-                int[]arr5 = new int[]{2,-12,9,3,1,6};
-                int[]arr6 = new int[]{-1,1,2,3};
-                return new Object[][]{{arr4},{arr5},{arr6}};
             case "arrayOutputTest":
                 int[] input = new int[] { 4, 3, 9, 7, 2, 1 };
                 int[] expected = new int[] { 2, 9, 3, 49, 4, 1 };
@@ -37,11 +28,20 @@ public class SquareRootTest {
                 int[]  input3 = new int[] { 1, 2, 3, 4, 5, 6 };
                 int[]  expected3 = new int[] { 1, 4, 9, 2, 25, 36 };
                 return new Object[][]{{input, expected},{input2,expected2},{input3,expected3}};
+            case "taskSizeArrayArgumentExceptionTest":
+                return new Object[][]{{"-2"}, {"-5"}, {"-3"}, {"-10"}};
+            case "taskArrayInputArgumentExceptionTest":
+                int[] input4 = new int[] { 4, 3, -9, 7, -2, 1 };
+                int[]  input5 = new int[] { 100, -101, 5, 5, -1, 1 };
+                int[]  input6 = new int[] { 1, -2, 3, 4, 5, 6 };
+                return new Object[][]{{"6",input4},{"6",input5},{"6",input6}};
             default:
                 return null;
         }
 
     }
+    @Mock
+    BufferedReader reader = Mockito.mock(BufferedReader.class);
     @Test(dataProvider = "dataProvider")
     public static void isPerfectSquareNegativeTest(int value)
     {
@@ -52,32 +52,24 @@ public class SquareRootTest {
     {
         Assert.assertTrue(SquareRoot.getIsPerfectSquare(value));
     }
-    @Test(dataProvider = "dataProvider")
-    public static void validateSizePositiveTest(int value)
-    {
-        Assert.assertTrue(SquareRoot.getValidateSize(value));
-    }
-    @Test(dataProvider = "dataProvider")
-    public static void validateSizeNegativeTest(int value)
-    {
-        Assert.assertFalse(SquareRoot.getValidateSize(value));
-    }
-    @Test(dataProvider = "dataProvider")
-    public static void validateArrayInputPositiveTest(int[]value)
-    {
-        Assert.assertTrue(SquareRoot.getValidateArrayInput(value.length,value));
-    }
-    @Test(dataProvider = "dataProvider")
-    public static void validateArrayInputNegativeTest(int[]value)
-    {
-        Assert.assertFalse(SquareRoot.getValidateArrayInput(value.length,value));
-    }
+
     @Test(dataProvider = "dataProvider")
     public static void arrayOutputTest(int[]value, int[] expectRes)
     {
         int[] output = new int[value.length];
         SquareRoot.getArrayOutput(value.length,value,output);
         Assert.assertEquals(output,expectRes);
+    }
+    @Test(expectedExceptions = IllegalArgumentException.class, dataProvider = "dataProvider")
+    public void taskSizeArrayArgumentExceptionTest(String size) throws IOException {
+        Mockito.when(reader.readLine()).thenReturn(size, null);
+        SquareRoot.task(reader);
+    }
+    @Test(expectedExceptions = IllegalArgumentException.class, dataProvider = "dataProvider")
+    public void taskArrayInputArgumentExceptionTest(String size,int [] data) throws IOException {
+        Mockito.when(reader.readLine()).thenReturn(size, null);
+        Mockito.when(reader.readLine()).thenReturn(Arrays.toString(data), null);
+        SquareRoot.task(reader);
     }
 
 }
