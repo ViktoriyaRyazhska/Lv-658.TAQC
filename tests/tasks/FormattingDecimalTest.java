0b149413a21eedf0620910999868fc8dbd328736
Roman Kaniuka <kanuka878@gmail.com>
2022-01-10 18:19:01 +0200

    Added some tests for 'FormattingDecimal.java'

--- tests/tasks/FormattingDecimalTest.java
+++ tests/tasks/FormattingDecimalTest.java
@@ -1,7 +1,33 @@
 package tasks;
 
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+
 import static org.testng.Assert.*;
 
 public class FormattingDecimalTest {
+    @DataProvider
+    private Object[][] dataProvider() {
+        return new Object[][] {
+                {1, 1},
+                {1, 1},
+                {1, 1},
+                {1, 1}
+        };
+    }
 
+    @Mock
+    BufferedReader reader = Mockito.mock(BufferedReader.class);
+
+    @Test(dataProvider = "dataProvider")
+    public void formattingTest(double number, double expected) {
+        double actual = FormattingDecimal.getFormatting(number);
+
+        Assert.assertEquals(actual, expected);
+    }
 }
