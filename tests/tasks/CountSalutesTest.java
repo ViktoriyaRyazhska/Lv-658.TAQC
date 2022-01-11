f6b716bdca81ff018d1a5fa24a751772c293e3a3
Roman Kaniuka <kanuka878@gmail.com>
2022-01-11 03:05:03 +0200

    Added some more tests for 'CountSalutes.java'

--- tests/tasks/CountSalutesTest.java
+++ tests/tasks/CountSalutesTest.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import com.sun.source.tree.ModuleTree;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.testng.Assert;
@@ -7,6 +8,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.BufferOverflowException;
 
 import static org.testng.Assert.*;
@@ -34,5 +36,18 @@ public class CountSalutesTest {
         Assert.assertEquals(actual, expected);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void invalidDataTest1() throws IOException {
+        Mockito.when(reader.readLine()).thenReturn("", null);
+
+        CountSalutes.task(reader);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void invalidDataTest2() throws IOException {
+        Mockito.when(reader.readLine()).thenReturn("-><-))", null);
+
+        CountSalutes.task(reader);
+    }
 
 }
