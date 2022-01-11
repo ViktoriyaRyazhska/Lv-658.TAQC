b9f556f4b58874a6c681962cd3d9e36b5983206a
Marta Khoma <tyxa.khoma16@gmail.com>
2022-01-11 04:23:54 +0300

    Willson's Prime task was updated

--- src/tasks/Seventh.java
+++ src/tasks/Seventh.java
@@ -3,40 +3,8 @@ package tasks;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
-import java.util.stream.LongStream;
 
 public class Seventh {
-    public static long factorialUsingStreams(int n) {
-        return LongStream.rangeClosed(1, n)
-                .reduce(1, (long x, long y) -> x * y);
-    }
-
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-//    public static boolean am_i_wilson(double n) {
-//        if(n == 563)
-//            return true;
-//        if(isPrime((int) n))
-//        {
-//            double check = factorialUsingStreams((int)(n-1))/Math.pow(n,2);
-//            return Math.ceil(check) != Math.floor(check);
-//        }
-//
-//        else
-//            return false;
-//    }
-
-
     public static boolean am_i_wilson(double n) {
         int input = (int) n;
         if (!BigInteger.valueOf(input).isProbablePrime(100)) {
@@ -52,14 +20,5 @@ public class Seventh {
             result = result.multiply(BigDecimal.valueOf(i));
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(am_i_wilson(5));
-        System.out.println(am_i_wilson(7));
-
-        System.out.println(am_i_wilson(13));
-        System.out.println(am_i_wilson(563));
-        //System.out.println(factorialUsingStreams(562));
     }
 }
